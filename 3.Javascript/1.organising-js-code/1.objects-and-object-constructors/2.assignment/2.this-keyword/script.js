f2ff71f44490b5ae00509138c5a05b37e6969b31
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-01-09 16:09:15 +0000

    Prototype & this Exercises

---
+++ 3.Javascript/1.organising-js-code/1.objects-and-object-constructors/2.assignment/2.this-keyword/script.js
@@ -0,0 +1,10 @@
+/* 
+This, is the context of a invocation
+JS has 4 invocation types:
+    Function Invocation: alert('Hello Wordl!')
+    Method Invocation: console.log('Hello World!')
+    Constructor Invocation: new RegExp('//d')
+    Indirect Invocation: alert.call(undefined, 'Hello World!')
+*/
+
+// Function Invocation
