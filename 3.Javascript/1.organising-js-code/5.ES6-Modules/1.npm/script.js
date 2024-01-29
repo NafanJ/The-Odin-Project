5818b7ab2b80be73a489637426c2ecdd43fc0cc3
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-01-29 11:48:23 +0000

    Webpack

--- 3.Javascript/1.organising-js-code/5.ES6-Modules/1.npm/script.js
+++ 3.Javascript/1.organising-js-code/5.ES6-Modules/1.npm/script.js
@@ -1,2 +1,8 @@
+// npm install webpack webpack-cli --save-dev
+
+//  ./node_modules/.bin/webpack ./index.js --mode=development
+// Looks for require, creates dist directory and main.js file
+var moment = require('moment');
+
 console.log("Hello from JavaScript!");  
 console.log(moment().startOf('day').fromNow());
