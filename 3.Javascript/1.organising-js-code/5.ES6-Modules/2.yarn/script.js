ec87ced0b95806825898ab0024b24eaa6d4747ec
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-01-29 13:07:20 +0000

    Yarn Comment

--- 3.Javascript/1.organising-js-code/5.ES6-Modules/2.yarn/script.js
+++ 3.Javascript/1.organising-js-code/5.ES6-Modules/2.yarn/script.js
@@ -1,1 +1,7 @@
-At some point, you will probably run into Yarn - a replacement for the default npm. For the most part, it does the same things, though it does have a few more features. Recent versions of npm have incorporated some of the best features of Yarn, so using it won’t offer you any real advantages at this point in your career. It is a fine package manager, however, and may be worth your consideration in the future.
+/*
+At some point, you will probably run into Yarn - a replacement for the default npm. 
+For the most part, it does the same things, though it does have a few more features. 
+Recent versions of npm have incorporated some of the best features of Yarn, 
+so using it won’t offer you any real advantages at this point in your career. 
+It is a fine package manager, however, and may be worth your consideration in the future.
+*/
