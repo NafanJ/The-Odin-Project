a41fd1002db7011803358f35b9c92731f3943d5b
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-02-22 16:15:16 +0000

    Renamed Directories

--- 5.React/3.functional-components/src/main.jsx
+++ 5.React/3.functional-components/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Filename - index.js
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom";
+import Example from "./App";
+
+const root = ReactDOM.createRoot(
+	document.getElementById("root")
+);
+root.render(
+	<React.StrictMode>
+		<Example />
+	</React.StrictMode>
+);
