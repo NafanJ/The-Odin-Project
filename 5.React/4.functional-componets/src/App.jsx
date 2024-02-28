dbdce2f03e704c8a004ac7a8d88be413e1231160
Nathan Joel <nathanjoel1@hotmail.co.uk>
2024-02-28 15:19:40 +0000

    Condtional Rendering

--- 5.React/4.functional-componets/src/App.jsx
+++ 5.React/4.functional-componets/src/App.jsx
@@ -6,6 +6,14 @@ function ListItem(props){
 
 
 function List(props){
+  if (!props.animals) {
+    return <div>Loading...</div>;
+  }
+
+  if (props.animals.length === 0) {
+    return <div>There are no animals in the list!</div>;
+  }
+
   return (
     <ul>
       {props.animals.map((animal) => {
